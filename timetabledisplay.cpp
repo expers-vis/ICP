7352ca34c9fbd0ed1d7334322e38cc7f8164d32e
expers-vis <32218820+expers-vis@users.noreply.github.com>
2020-05-13 01:41:14 +0200

    lepší vypis

--- timetabledisplay.cpp
+++ timetabledisplay.cpp
@@ -47,43 +47,55 @@ void timetableDisplay::displayTimetable(QString t_filename, int line_id) {
     QFile timetable_file("timetables/" + t_filename);
     QFile bus_file("data/buses.txt");
 
-    if (timetable_file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+    /* open files */
+    if (timetable_file.open(QIODevice::ReadOnly | QIODevice::Text) && bus_file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         QTextStream t_stream(&timetable_file);
+        QTextStream b_stream(&bus_file);
+        QString printLine;      // output line
+
+        /* print header with stop names */
+        printLine.sprintf("%6s\t", " ");
 
         while(!t_stream.atEnd()) {
             QString t_line = t_stream.readLine();
             QList t_list = t_line.split(" ");
 
-            QString printLine;
-            printLine.sprintf("%20s", t_list.at(0).toLocal8Bit().constData());
-            int stop_time = QString(t_list.at(1).toLocal8Bit().constData()).toInt();
+            printLine.append(QString().sprintf("%*s\t", t_list.at(0).size(), t_list.at(0).toLocal8Bit().constData()));
+        }
+        ui->timetableList->addItem(printLine);  // print line
+        t_stream.seek(0);   // reset stream
 
+        /* print arrival time for each bus */
+        int bus_num = 0;    // bus number within line
 
-            if(bus_file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-                QTextStream b_stream(&bus_file);
+        while(!b_stream.atEnd()) {
+            QString b_line = b_stream.readLine();
+            QList b_list = b_line.split(" ");
 
-                while(!b_stream.atEnd()) {
-                    QString b_line = b_stream.readLine();
-                    QList b_list = b_line.split(" ");
+            /* print buses only for requested line */
+            int bus_id = QString(b_list.at(0).toLocal8Bit().constData()).toInt();
+            if(bus_id == line_id) {
+                printLine.sprintf("%s #%d\t", "Bus", ++bus_num);
 
-                    int bus_id = QString(b_list.at(0).toLocal8Bit().constData()).toInt();
+                int bus_time = QString(b_list.at(1).toLocal8Bit().constData()).toInt();
 
-                    if(line_id == bus_id) {
-                        printLine.append("\t");
+                /* append arrival to the stop */
+                while(!t_stream.atEnd()) {
+                    QString t_line = t_stream.readLine();
+                    QList t_list = t_line.split(" ");
 
-                        int bus_time = QString(b_list.at(1).toLocal8Bit().constData()).toInt();
+                    int stop_time = QString(t_list.at(1).toLocal8Bit().constData()).toInt();
+                    QString time = formatTime(stop_time + bus_time);
 
-                        printLine.append(formatTime(stop_time + bus_time));
-                    }
+                    printLine.append(QString().sprintf("%*s\t", time.size(),time.toLocal8Bit().constData()));
+
                 }
 
-                //b_stream.seek(0);
+                t_stream.seek(0);
 
+                /* print bus to list */
+                ui->timetableList->addItem(printLine);
             }
-            bus_file.close();
-
-            /* print line to list */
-            ui->timetableList->addItem(printLine);
         }
 
     }
@@ -92,14 +104,16 @@ void timetableDisplay::displayTimetable(QString t_filename, int line_id) {
         exit(1);
     }
 
+    ui->timetableList->setMinimumWidth(ui->timetableList->sizeHintForColumn(0) + 2 * ui->timetableList->frameWidth());    // adjust list width
+    ui->timetableList->setMinimumHeight(ui->timetableList->sizeHintForRow(0) * ui->timetableList->count() + 2 * ui->timetableList->frameWidth());
     timetable_file.close();
+    bus_file.close();
 }
 
 inline QString timetableDisplay::formatTime(long int sec) {
     int hour = sec / 3600;
     sec = sec % 3600;
     int min = sec / 60;
-    sec = sec % 60;
 
-    return QString().sprintf("%02d:%02d:%02ld", hour, min, sec);
+    return QString().sprintf("%02d:%02d", hour, min);
 }
